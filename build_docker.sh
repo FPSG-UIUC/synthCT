37104d5d246b899c5d6e0fa25c323442c6437a11
Sushant Dinesh <sushant.dinesh94@gmail.com>
2022-10-04 17:27:03 -0500

    Fixes filename in build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git archive -o docker/dsa-fuzz.tar.gz master
+git archive -o docker/synthCT.tar.gz master
 tar -cf docker/X86-64-semantics.tar.gz third-party/X86-64-semantics/semantics
 
 cd docker/
